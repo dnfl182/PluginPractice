656320eda9a0ab7b52c1b62b095b9f4cbbc6b36e
닭강정 <@211.230.34.2>
2016-07-06 13:37:54 +0900

    Plugin File

---
+++ mine/src/mine/Main.java
@@ -0,0 +1,13 @@
+package mine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	public void onEnable(){
+		getLogger().info("minecraft is good game do you know this fact?");
+	}
+	public void onDisable(){
+		
+	}
+}
+	
